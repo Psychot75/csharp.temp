1e1ba1a79033558cd8b379e98da530bde903f8ca
Etienne Tardif <etiennetardif24@gmail.com>
2025-12-25 16:09:17 -0500

    push

--- Arcraven.WebRtc/IVideoFrameSink.cs
+++
@@ -1,9 +0,0 @@
-namespace Arcraven.WebRtc;
-
-public interface IVideoFrameSink
-{
-    /// <summary>
-    /// Push a BGRA32 frame.
-    /// </summary>
-    void OnFrameBgra32(ReadOnlySpan<byte> bgra, int width, int height, int strideBytes);
-}
